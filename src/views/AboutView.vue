1bce78a117c169363e40c53be0883a45b32b6f36
RicardoRonan <ricardoronanm@gmail.com>
2022-07-14 13:24:45 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <carrousel />
 </template>
+<script>
+import carrousel from "../components/carrousel.vue";
+export default {
+  components: { carrousel },
+};
+</script>
